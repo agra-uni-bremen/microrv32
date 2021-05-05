7294170cee6cd291c640dc4abce368d4324bcba6
Sallar Ahmadi-Pour <sallar@uni-bremen.de>
2021-05-05 11:45:46 +0200

    gpio peripheral + testbench for scenarios

--- microrv32/src/main/scala/core/microrv32/peripheral/gpio/GPIOBank.scala
+++ microrv32/src/main/scala/core/microrv32/peripheral/gpio/GPIOBank.scala
@@ -3,15 +3,13 @@ package core.microrv32.peripheral.gpiobank
 import spinal.core._
 import spinal.lib.slave
 import spinal.lib._
+import spinal.lib.io.{InOutWrapper, TriStateArray}
 import core.microrv32.SimpleBus
 
-class GPIOBank() extends Component{
+class SBGPIOBank() extends Component{
     val io = new Bundle{
         val sb = slave(SimpleBus(32,32))
-        //val gpio = master(TriStateArray(8 bits)) // this is on toplevel instead
-        val gpioInput = in(Bits(8 bits))
-        val gpioDir = out(Bits(8 bits))
-        val gpioOut = out(Bits(8 bits))
+        val gpio = master(TriStateArray(8 bits))
         val sel = in(Bool)
     }
     val rdy = Reg(Bool) init(False)
@@ -22,13 +20,13 @@ class GPIOBank() extends Component{
     val SBGPIOLogic = new Area{
         intAddr := io.sb.SBaddress(7 downto 0)
         
-        directionReg := Reg(8 bits) init(0)
-        outputReg := Reg(8 bits) init(0)
+        val directionReg = Reg(Bits(8 bits)) init(0)
+        val outputReg = Reg(Bits(8 bits)) init(0)
         // buffer for 2 clocks to get rid of meta stability
-        inputStage := RegNext(io.gpioInput)
-        inputReg := RegNext(inputStage)
-        io.gpioDir := directionReg
-        io.gpioOut := outputReg
+        val inputStage = RegNext(io.gpio.read)
+        val inputReg = RegNext(inputStage)
+        io.gpio.writeEnable := directionReg
+        io.gpio.write := outputReg
         
         val sbDataOutputReg = Reg(Bits(32 bits)) init(0)
         sbDataOutputReg := 0
@@ -57,6 +55,5 @@ class GPIOBank() extends Component{
         rdy := True
     }
     io.sb.SBready := rdy
-    //io.sb.SBrdata := Mux(SBUartLogic.outputRegSel,SBUartLogic.outputBuff,SBUartLogic.sbDataOutputReg)
-    io.sb.SBrdata := sbDataOutputReg
+    io.sb.SBrdata := SBGPIOLogic.sbDataOutputReg
 }
--- microrv32/src/main/scala/core/microrv32/peripheral/gpio/GPIOBankSim.scala
+++ microrv32/src/main/scala/core/microrv32/peripheral/gpio/GPIOBankSim.scala
@@ -5,7 +5,7 @@ import spinal.sim._
 import spinal.core.sim._
 import scala.math._
 
-object SBUartTopSim {
+object GPIOBankSim {
 
   var simEndTime : BigInt = 0
 
@@ -52,8 +52,13 @@ object SBUartTopSim {
         dut.clockDomain.forkStimulus(period = 82)
         var dutRunning = false
         var simSteps : Int = 0
-        var maxsimSteps : Int = 1200
+        var maxsimSteps : Int = 100
         
+        var gpioOutVar : Int = 0
+        var gpioOutput : Int = 0
+        var gpioInVar : Int = 0
+        var gpioInput : BigInt = 0
+
         // starting values
         dut.io.sb.SBaddress #= 0
         dut.io.sb.SBvalid #= false
@@ -61,14 +66,110 @@ object SBUartTopSim {
         dut.io.sb.SBwrite #= false
         dut.io.sb.SBsize #= 4
         dut.io.sel #= false
-        // dut.io.uart.rxd #= true
-        dut.clockDomain.waitRisingEdge()
         
-        while(simSteps <= maxsimSteps){
-          //Wait a rising edge on the clock
+        dut.io.gpio.read #= 0
+
+        // T1: write all values to output reg with all pins enabled 
+        dut.clockDomain.waitRisingEdge()
+        println("T1: write all values to output reg with all pins enabled")
+        write(0, 0x000000FF)
+        for(i <- 0x00 to 0xff)
+        {
+          gpioOutVar = i
           dut.clockDomain.waitRisingEdge()
-          simSteps += 1
+          write(4, gpioOutVar)
+          dut.clockDomain.waitRisingEdge()
+          gpioOutput = dut.io.gpio.write.toInt & dut.io.gpio.writeEnable.toInt
+          if(gpioOutput != i)
+          {
+            printf("T1: GPIO Output not correct @ %h\n", i)
+          }
+          dut.clockDomain.waitRisingEdge()
         }
+
+        // T2: write all values to output reg with masked output enable
+        dut.clockDomain.waitRisingEdge()
+        println("T2: write all values to output reg with masked output enable")
+        write(0, 0x00000055)
+        for(i <- 0x00 to 0xff)
+        {
+          gpioOutVar = i & 0x55
+          dut.clockDomain.waitRisingEdge()
+          write(4, gpioOutVar)
+          dut.clockDomain.waitRisingEdge()
+          gpioOutput = dut.io.gpio.write.toInt & dut.io.gpio.writeEnable.toInt
+          if(gpioOutput != (i & 0x55))
+          {
+            printf("T2: GPIO Output not correct @ %h\n", i)
+          }
+          dut.clockDomain.waitRisingEdge()
+        }
+        
+        // T3: write all values to output reg with no output enabled
+        dut.clockDomain.waitRisingEdge()
+        println("T3: write all values to output reg with no output enabled")
+        write(0, 0x00000000)
+        write(4, 0x00000000)
+        for(i <- 0x00 to 0xff)
+        {
+          gpioOutVar = i & 0xff
+          dut.clockDomain.waitRisingEdge()
+          write(4, gpioOutVar)
+          dut.clockDomain.waitRisingEdge()
+          gpioOutput = dut.io.gpio.write.toInt & dut.io.gpio.writeEnable.toInt
+          if(gpioOutput != (i & 0x00))
+          {
+            printf("T3: GPIO Output not correct @ %h\n", i)
+          }
+          dut.clockDomain.waitRisingEdge()
+        }
+
+        // T4: read in values through gpio and compare input register with input (all pins read)
+        dut.clockDomain.waitRisingEdge()
+        println("T4: read in values through gpio and compare input register with input")
+        write(0, 0x00000000)
+        var readMask = 0xff
+        for(i <- 0x00 to 0xff)
+        {
+          gpioInVar = i & readMask
+          dut.io.gpio.read #= gpioInVar
+          dut.clockDomain.waitRisingEdge()
+          dut.clockDomain.waitRisingEdge()
+          gpioInput = read(8).toBigInt & 0xff
+          // dut.clockDomain.waitRisingEdge()
+          if((gpioInput & readMask) != gpioInVar)
+          {
+            printf("T4: GPIO Input not correct @ %h\n", i)
+          }
+          dut.clockDomain.waitRisingEdge()
+        }
+
+        // T5: read in values through gpio and compare input register with input (masked enable)
+        dut.clockDomain.waitRisingEdge()
+        println("T5: read in values through gpio and compare input register with input (masked enable)")
+        write(0, 0x00000000)
+        readMask = 0xAA
+        for(i <- 0x00 to 0xff)
+        {
+          gpioInVar = i & readMask
+          dut.io.gpio.read #= gpioInVar
+          dut.clockDomain.waitRisingEdge()
+          dut.clockDomain.waitRisingEdge()
+          gpioInput = read(8).toBigInt & 0xff
+          // dut.clockDomain.waitRisingEdge()
+          if((gpioInput & readMask) != gpioInVar)
+          {
+            printf("T5: GPIO Input not correct @ %h\n", i)
+          }
+          dut.clockDomain.waitRisingEdge()
+        }
+
+        // while(simSteps <= maxsimSteps){
+        //   //Wait a rising edge on the clock
+        //   dut.clockDomain.waitRisingEdge()
+        //   simSteps += 1
+        // }
+
         dut.clockDomain.waitRisingEdge()
       }
   }
