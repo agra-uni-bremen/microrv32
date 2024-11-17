ce4afb48b1710408d0a75d5eb934e718c2482966
RuikeDeng <dengruike88@gmail.com>
2024-11-17 20:02:13 +0100

    Change the summary format from .log to .csv.

--- microrv32/extractLogs.sh
+++ microrv32/extractLogs.sh
@@ -3,10 +3,12 @@
 shopt -s extglob
 
 extractLogsToCSV() {
-	echo "benchmark; host-cpu-time-ns; instruction-count; cycle-count; cpi; ipc; ips" > sw/embench-split-prebuilt/summary.log
+	# echo "benchmark; host-cpu-time-ns; instruction-count; cycle-count; cpi; ipc; ips" > sw/embench-split-prebuilt/summary.log
+	echo "benchmark; host-cpu-time-ns; instruction-count; cycle-count; cpi; ipc; ips" > sw/embench-split-prebuilt/summary.csv
 	for benchmarkPath in sw/embench-split-prebuilt/!(huffbench|md5sum|qrduino|picojpeg|sglib-combined|tarfind|wikisort); do
 		benchmarkName=$(basename $benchmarkPath)
-		if [ "${benchmarkName}" != "summary.log" ]; then
+		# if [ "${benchmarkName}" != "summary.log" ]; then
+		if [ "${benchmarkName}" != "summary.csv" ]; then
 			echo $benchmarkName
 			data=$(awk -f extractLogs.awk ${benchmarkPath}/simulation.log)
 			echo "${benchmarkName}; ${data}" >> sw/embench-split-prebuilt/summary.csv
