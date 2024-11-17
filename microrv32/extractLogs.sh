f2961daa4fadfa62fb886198089871f674fcee3f
RuikeDeng <dengruike88@gmail.com>
2024-11-17 20:14:19 +0100

    Add the 'Data' and 'Configuration' to the summary.

--- microrv32/extractLogs.sh
+++ microrv32/extractLogs.sh
@@ -3,8 +3,8 @@
 shopt -s extglob
 
 extractLogsToCSV() {
-	# echo "benchmark; host-cpu-time-ns; instruction-count; cycle-count; cpi; ipc; ips" > sw/embench-split-prebuilt/summary.log
-	echo "benchmark; host-cpu-time-ns; instruction-count; cycle-count; cpi; ipc; ips" > sw/embench-split-prebuilt/summary.csv
+	# echo "benchmark; host-cpu-time-ns; instruction-count; cycle-count; cpi; ipc; ips; Date; Configuration" > sw/embench-split-prebuilt/summary.log
+	echo "benchmark; host-cpu-time-ns; instruction-count; cycle-count; cpi; ipc; ips; Date; Configuration" > sw/embench-split-prebuilt/summary.csv
 	for benchmarkPath in sw/embench-split-prebuilt/!(huffbench|md5sum|qrduino|picojpeg|sglib-combined|tarfind|wikisort); do
 		benchmarkName=$(basename $benchmarkPath)
 		# if [ "${benchmarkName}" != "summary.log" ]; then
